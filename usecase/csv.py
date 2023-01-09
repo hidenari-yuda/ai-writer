58e1ceb4fbe0e01a49adec4f9770ffdb7abf5856
Hidenari-Yuda <106872451+hidenari-yuda@users.noreply.github.com>
2023-01-10 01:35:36 +0900

    fix: gitignore

--- usecase/csv.py
+++ usecase/csv.py
@@ -5,7 +5,7 @@ import datetime
 def write_csv(service_name, data):
     # print now time
     datetime_now = datetime.datetime.now().strftime('%Y%m%d%H%M%S')
-    file_name = service_name+datetime_now+'.csv'
+    file_name = './tmp/'+service_name+datetime_now+'.csv'
     with open(file_name, 'w', newline='') as f:
         writer = csv.writer(f)
         writer.writerows(data)
